8e7d4b6df12e90f640501d802a7efe39b3ee5408
daphne.rouw <daphne@vinomofo.com>
2017-09-12 10:26:34 +1000

    Save messages

--- web/channels/elixirgirls_channel.ex
+++ web/channels/elixirgirls_channel.ex
@@ -16,19 +16,24 @@ defmodule Slackir.ElixirGirlsChannel do
   # It is also common to receive messages from the client and
   # broadcast to everyone in the current topic (random:lobby).
   def handle_in("new_message", message, socket) do
-    changeset = Message.changeset(%Message{}, message)
+    spawn_link(__MODULE__, :save_message, [message])
+    broadcast socket, "new_message", message
+    {:reply, :ok, socket}
 
-    if changeset.valid? do
-      Repo.insert!(changeset)
-      broadcast socket, "new_message", message
-      {:reply, :ok, socket}
-    else
-      {:reply, :error, socket}
-    end
   end
 
   # Add authorization logic here as required.
   defp authorized?(_payload) do
     true
+  end
+
+  def save_message(message) do
+    changeset = Message.changeset(%Message{}, message)
+
+    if changeset.valid? do
+      Repo.insert!(changeset)
+    else
+      false
+    end
   end
 end
